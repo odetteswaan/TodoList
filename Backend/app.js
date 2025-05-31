9af01fff25580ae6e9c53cce3133decccee09577
Akash rathour <113913977+odetteswaan@users.noreply.github.com>
2025-05-31 21:48:33 +0530

    added cors middle ware

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser')
 const express=require('express')
 const Router=require('./Routes')
+const cors=require('cors')
 const { default: mongoose } = require('mongoose')
 
 const app=express()
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/api',Router)
 
 mongoose.connect('mongodb+srv://akash:17082001@cluster0.fo2hosk.mongodb.net/Task?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
